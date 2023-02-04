5c681e9eb0e179b4d884f578f1945572d9022bca
Bryan <bryanpool991@hotmail.com>
2023-02-04 17:52:20 -0500

    script donde se encunetra todo la funcionalidad

---
+++ blogr-landing-page/src/App.js
@@ -0,0 +1,15 @@
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+const App=()=>{
+    return(
+        <div>
+            <Header
+                logo="logo"
+            />
+            <Main />
+            <Footer />
+        </div>
+    );
+}
+export default App;
