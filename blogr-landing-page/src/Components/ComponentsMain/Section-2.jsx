5be82bd328b5b2784349c016bead4bd30c06688f
BryanPool99 <bryanpool991@hotmail.com>
2023-02-04 20:59:29 -0500

    componentes del main hecho en react

--- blogr-landing-page/src/Components/ComponentsMain/Section-2.jsx
+++ blogr-landing-page/src/Components/ComponentsMain/Section-2.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import imgS2 from '../../Images/illustration-phones.svg';
 const Section2 = ()=>{
     return(
-        <div>SECTION 2</div>
+        <div>
+            <div>
+                <img src={imgS2} alt='illustration-phones'/>
+            </div>
+            <div>
+                <h2>State of the Art Infrastructure</h2>
+                <p>
+                    With reliability and speed in mind, worldwide data centers provide the backbone for ultra-fast connectivity. 
+                    This ensures your site will load instantly, no matter where your readers are, keeping your site competitive.
+                </p>
+            </div>
+        </div>
     );
 }
 export default Section2;
--- blogr-landing-page/src/Components/ComponentsMain/Section-3.jsx
+++ blogr-landing-page/src/Components/ComponentsMain/Section-3.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import imgS3 from '../../Images/illustration-laptop-desktop.svg';
 const Section3 = ()=>{
     return(
-        <div>SECTION 3</div>
+        <div>
+            <div>
+                <img src={imgS3} alt='illustration-laptop'/>
+            </div>
+            <div>
+                <h3>Free, open, simple</h3>
+                <p>
+                    Blogr is a free and open source application backed by a large community of helpful developers. It supports 
+                    features such as code syntax highlighting, RSS feeds, social media integration, third-party commenting tools, 
+                    and works seamlessly with Google Analytics. The architecture is clean and is relatively easy to learn.
+                </p>
+                <h3>Powerful tooling</h3>
+                <p>
+                    Batteries included. We built a simple and straightforward CLI tool that makes customization and deployment a breeze, but
+                    capable of producing even the most complicated sites.
+                </p>
+            </div>
+        </div>
     );
 }
 export default Section3;
