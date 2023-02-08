9fd0828f74d541254847e34e0f29d0ca9cdc0604
Bryan <bryanpool991@hotmail.com>
2023-02-08 12:23:33 -0500

    estructura del main jsx

---
+++ fylo-data-storage/src/Components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContainerIcons from './ComponentsMain/ContainerIcons';
+import ContainerInfo from './ComponentsMain/ContainerInfo';
+const Main=()=>{
+    return (
+        <main>
+            <ContainerIcons/>
+            <ContainerInfo/>
+        </main>
+    )
+}
+export default Main;
