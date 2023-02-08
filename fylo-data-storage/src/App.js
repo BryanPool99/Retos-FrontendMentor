d31ff3e10b872024a8593d0f0582f8759ae36419
Bryan <bryanpool991@hotmail.com>
2023-02-08 12:25:54 -0500

    estructura principal del proyecto hecho en react

---
+++ fylo-data-storage/src/App.js
@@ -0,0 +1,13 @@
+import './index.css'
+import Main from './Components/Main';
+import Footer from './Components/Footer';
+function App() {
+  return (
+    <>
+      <Main />
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
